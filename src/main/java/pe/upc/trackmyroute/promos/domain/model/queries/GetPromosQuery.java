513c7edaee89ff28feb894b3c6212d893f1fe8a8
Javier Sebastian <u20211e429@upc.edu.pe>
2024-09-26 12:47:21 -0500

    feat: add backend

---
+++ src/main/java/pe/upc/trackmyroute/promos/domain/model/queries/GetPromosQuery.java
@@ -0,0 +1,4 @@
+package pe.upc.trackmyroute.promos.domain.model.queries;
+
+public record GetPromosQuery() {
+}
---
+++ src/test/java/pe/upc/trackmyroute/TrackMyRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.upc.trackmyroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackMyRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
