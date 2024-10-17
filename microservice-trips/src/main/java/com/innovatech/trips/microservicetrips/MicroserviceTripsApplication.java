8770ba63afd4910fc94b1578d2aa7d6bde309bc4
JorgeQuilla <u20211b197@upc.edu.pe>
2024-10-17 13:24:08 -0500

    feat: trips module added

---
+++ microservice-trips/src/main/java/com/innovatech/trips/microservicetrips/MicroserviceTripsApplication.java
@@ -0,0 +1,13 @@
+package com.innovatech.trips.microservicetrips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceTripsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceTripsApplication.class, args);
+	}
+
+}
