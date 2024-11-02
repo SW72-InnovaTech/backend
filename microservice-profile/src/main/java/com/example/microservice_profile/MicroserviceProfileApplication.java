a996742318cc7b2f25d72ed26e2002f259371d2e
Julio Elsner <u202111654@upc.edu.pe>
2024-11-02 11:57:59 -0500

    chore: created profile microservice

---
+++ microservice-profile/src/main/java/com/example/microservice_profile/MicroserviceProfileApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservice_profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceProfileApplication.class, args);
+	}
+
+}
