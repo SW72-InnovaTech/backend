1b12988df69142ddb657cd14030d528eabd2aa2d
Javier Sebastian <u20211e429@upc.edu.pe>
2024-11-03 20:53:10 -0500

    feat: added link notification microservice

--- microservice-notification/src/main/java/com/microservice/shared/domain/model/aggregate/AuditableAbstractAggregateRoot.java
+++ microservice-notification/src/main/java/com/microservice/shared/domain/model/aggregate/AuditableAbstractAggregateRoot.java
@@ -2,8 +2,6 @@ package com.microservice.shared.domain.model.aggregate;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.domain.AbstractAggregateRoot;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
