76a201c27f311cf803f58210945a190cd16f22dd
JorgeQuilla <u20211b197@upc.edu.pe>
2024-11-21 11:28:45 -0500

    chore: dockerfiles added

--- microservice-profile/src/test/java/com/innovatech/microserviceprofile/MicroserviceProfileApplicationTests.java
+++ microservice-profile/src/test/java/com/innovatech/microserviceprofile/MicroserviceProfileApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.microservice_profile;
+package com.innovatech.microserviceprofile;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
